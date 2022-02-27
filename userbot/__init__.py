493796f4f2d6b872375a339da3463ef9d49c1b3c
kenaja <99611859+kenaja@users.noreply.github.com>
2022-02-27 13:21:00 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -149,7 +149,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/PunyaAlby/ALBY-Userbot")
+    "https://github.com/kenaja/ALBY-Userbot")
 UPSTREAM_REPO_BRANCH = os.environ.get(
     "UPSTREAM_REPO_BRANCH", "ALBY-Userbot")
 
